5aa19dbe22888c312a02eaec8e5cffbfa7d5312e
Florian Rohrer <semicolonator@gmail.com>
2020-12-01 18:49:57 +0100

    day 1

--- day01/main.py
+++ day01/main.py
@@ -4,13 +4,10 @@ with open("input.txt") as f:
 for i, n1 in enumerate(l):
     for j, n2 in enumerate(l[i+1:]):
         if n1 + n2 == 2020:
-            part1 = n1 * n2
+            print(n1 * n2)
         for n3 in l[i+j+1:]:
             if n1 + n2 + n3 == 2020:
-                part2 = n1 * n2 * n3
-
-print(part1)
-print(part2)
+                print(n1 * n2 * n3)
 
 # Output:
 # 485739
