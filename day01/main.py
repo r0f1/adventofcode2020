09f569b8d916befaa798c2d5fa48adfe9d0bd4b0
Florian Rohrer <semicolonator@gmail.com>
2020-12-01 18:29:24 +0100

    day 1

--- day01/main.py
+++ day01/main.py
@@ -1,20 +1,16 @@
 with open("input.txt") as f:
     l = [int(x.strip()) for x in f]
 
-# Part 1
-for i, n1 in enumerate(l):
-    for n2 in l[i+1:]:
-        if n1 + n2 == 2020:
-            print(n1 * n2)
-            break
-
-# Part 2
 for i, n1 in enumerate(l):
     for j, n2 in enumerate(l[i+1:]):
+        if n1 + n2 == 2020:
+            part1 = n1 * n2
         for n3 in l[i+j+1:]:
             if n1 + n2 + n3 == 2020:
-                print(n1 * n2 * n3)
-                break
+                part2 = n1 * n2 * n3
+
+print(part1)
+print(part2)
 
 # Output:
 # 485739
