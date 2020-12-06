3ef9e8673db423b0be7ebf3de94af82933cf803b
Florian Rohrer <semicolonator@gmail.com>
2020-12-06 10:25:06 +0100

    day 6

--- day06/main.py
+++ day06/main.py
@@ -1,17 +1,7 @@
 from collections import Counter
 
 with open("input.txt") as f:
-    lines = [x.strip() for x in f]
-
-grps = []
-g = []
-for line in lines:
-    if len(line) == 0:
-        grps.append(g)
-        g = []
-    else:
-        g.append(line)
-grps.append(g)
+    grps = [x.strip().split() for x in f.read().split("\n\n")]
     
 print(sum([len(Counter("".join(g))) for g in grps]))
 print(sum([len([v for v in Counter("".join(g)).values() if v == len(g)]) for g in grps]))
