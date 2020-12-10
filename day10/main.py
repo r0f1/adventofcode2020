4011a5695ece9e99a34e58a95555a99e2ff5bd26
Florian Rohrer <r0f1@users.noreply.github.com>
2020-12-10 22:14:30 +0100

    Update main.py

--- day10/main.py
+++ day10/main.py
@@ -8,5 +8,7 @@ k = np.diff(numbers)
 print((1+(k == 1).sum()) * (1+(k == 3).sum()))
 
 cands = [e for i, e in run_length.encode(np.diff([0]+numbers)) if i == 1 and e > 1]
+# Tribonacci numbers - http://oeis.org/wiki/Tribonacci_numbers 
+# 0, 0, 1, 1, 2, 4, 7, 13, 24, 44, ...
 combinations = {2: 2, 3: 4, 4: 7}
 print(np.prod([combinations[e] for e in cands]))
