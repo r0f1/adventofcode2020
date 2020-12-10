a7459a3131da37e44fdd52b8ccf0c703a3fadcb8
Florian Rohrer <semicolonator@gmail.com>
2020-12-10 15:31:08 +0100

    Added alternative.

--- day10/main.py
+++ day10/main.py
@@ -5,13 +5,8 @@ with open("input.txt") as f:
     numbers = sorted([int(x) for x in f])
 
 k = np.diff(numbers)
-print((1+len(k[k == 1])) * (1+len(k[k == 3])))
+print((1+(k == 1).sum()) * (1+(k == 3).sum()))
 
-# 1 --> ignore
-# 2 --> 1, 0 (2)
-# 3 --> 1 1, 1 0, 0 1, 0 0 (4)
-# 4 --> 1 1 1, 1 0 0, 0 1 0, 0 0 1, 1 1 0, 1 0 1, 0 1 1 (7)
 cands = [e for i, e in run_length.encode(np.diff([0]+numbers)) if i == 1 and e > 1]
-t = {2: 2, 3: 4, 4: 7}
-transformed = [t[e] for e in cands]
-print(np.prod(transformed))
+combinations = {2: 2, 3: 4, 4: 7}
+print(np.prod([combinations[e] for e in cands]))
