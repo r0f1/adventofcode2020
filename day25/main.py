ddc261c6cfb8185fd6ec38c7ae8ddd185b43cbb1
Florian Rohrer <semicolonator@gmail.com>
2020-12-28 10:24:01 +0100

    day 25

--- day25/main.py
+++ day25/main.py
@@ -1,22 +1,10 @@
 with open("input.txt") as f:
-    public_key_card, public_key_door = [int(x.strip()) for x in f]
+    pub1, pub2 = [int(x) for x in f]
 
-def calc_loop_size(public_key):
-    subject_number = 7
-    n = 1
-    i = 0
-    while True:
-        n = (n * subject_number) % 20201227
-        if n == public_key:
-            return i+1
-        i += 1
+n = 1
+loop_size = 0
+while n != pub2:
+    n = (n * 7) % 20201227
+    loop_size += 1
 
-def calc_encryption_key(subject_number, loop_size):
-    n = 1
-    for i in range(loop_size):
-        n = (n * subject_number) % 20201227
-    return n
-
-loop_size = calc_loop_size(public_key_card)
-enc_key = calc_encryption_key(public_key_door, loop_size)
-print(enc_key)
+print(pow(pub1, loop_size, 20201227))
